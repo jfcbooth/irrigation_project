81cd7fc4c198e06fa292618d5edfbfb68c8d2585
theGyroid <theflyingmonkey1029@gmail.com>
2022-04-12 17:33:27 -0400

    Further arduino code development

--- waterer/waterer.ino
+++ waterer/waterer.ino
@@ -1,8 +1,11 @@
+// Includes
+#include <SD.h> // Sd card
+#include <SoftwareSerial.h> // Bluetooth module
+
 // Input
 #define TEMP
 #define HUMID
 #define MOIST
-#define H2O_SIGNAL
 #define DIAL_1
 #define DIAL_2
 #define DIAL_3
@@ -15,8 +18,15 @@
 #define DHT_SWITCH
 #define H2O_SWITCH
 #define LED
+#define SD_PIN
+
+// Global Variables
+File readLog;
+long moistIdeal;
 
 void setup() {
+  Serial.begin(9600);
+  
   // Input
   pinMode(TEMP, INPUT);
   pinMode(HUMID, INPUT);
@@ -33,11 +43,41 @@ void setup() {
   pinMode(DHT_SWITCH, OUTPUT);
   pinMode(H2O_SWITCH, OUTPUT);
   pinMode(LED, OUTPUT);
+
+  // Setup
+  if (digitalRead(DIAL_1)) {
+    moistIdeal = 0;
+  } else if(digitalRead(DIAL_2)) {
+    moistIdeal = 0;
+  } else if(digitalRead(DIAL_3)) {
+    moistIdeal = 0;
+  } else if(digitalRead(DIAL_4)) {
+    moistIdeal = 0;
+  } else if(digitalRead(DIAL_5)) {
+    moistIdeal = 0;
+  } else if(digitalRead(DIAL_6)) {
+    moistIdeal = 0;
+  } else if(digitalRead(DIAL_7)) {
+    moistIdeal = 0;
+  }
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+  long moistCurrent;
+  long humid;
+  long temp;
+
+  while(!SD.begin(SD_PIN)) {
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
+
+  readLog = SD.open("log.txt", FILE_WRITE);
+  moistCurrent = digitalRead(MOIST);
+  humid = digitalRead(HUMID);
+  temp = digitalRead(TEMP);
+
+  if (
 }
